3ddd2e4c6816d17e4ef4bb82ce155039d67a823b
jensenkokou <jensen.kokou@gmail.com>
2023-10-11 19:42:41 -0700

    S3 Module

--- main.tf
+++ main.tf
@@ -1,5 +1,19 @@
-resource "aws_instance" "test" {}
-
 module "s3" {
   source = "git@github.com:jensenkokou/s3-backend.git//s3-modules?ref=v1.1.0"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+ backend "s3" {
+    bucket = "landmark-automation-jensen"
+    key    = "action/terraform.tfstate"
+    region = "us-west-1" # region im creating my backend in 
+    encrypt = true
+  }
 }
